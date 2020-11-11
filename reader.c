86f518921a0456a0b21c2bce40753381750fb18b
Sergio Alberino <alberino@gmail.com>
2020-11-11 19:45:09 -0300

    fucionando

--- reader.c
+++ reader.c
@@ -17,23 +17,29 @@
 #define BUFFER_SIZE 300
 
 
-void recibiSignal(int sig)
+void Signal_receive(int sig)
 {
   FILE *Sp;
   /* Open signals.txt */
-	printf("abriendo archivo signals.txt...\n");
+	printf("saving in signals.txt...\n");
 	if ( (Sp = fopen(SIGFILE_NAME, "a") ) == NULL )
     {
         printf("Error opening signal file \n");
         exit(1);
     }
   
-  if (sig==SIGUSR1)
-  printf("SIGN:1 \n");
-  fprintf(Sp,"SIGN:1 \n");
-  if (sig==SIGUSR2)
-  printf("SIGN:2 \n");
-  fprintf(Sp,"SIGN:2 \n");
+    if (sig==SIGUSR1)
+    {
+        printf("SIGN:1 \n");
+        fprintf(Sp,"SIGN:1 \n");
+    }
+
+    if (sig==SIGUSR2)
+    {
+        printf("SIGN:2 \n");
+        fprintf(Sp,"SIGN:2 \n");
+    }
+    fclose(Sp);
 
 }
 
@@ -44,26 +50,24 @@ int main(void)
     FILE *fp;
     pid_t pid;
 
- /* Open log.txt */
-	printf("abriendo archivo log.txt...\n");
+	struct sigaction sa;
+	sa.sa_handler = Signal_receive;
+	//sa.sa_flags = 0; //SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1,&sa,NULL);
+    sigaction(SIGUSR2,&sa,NULL);
+ 
+    pid=getpid();      //Process ID of itself
+    printf("el pid es:  %d  \n", pid);
+
+
+    /* Open log.txt */
+	printf("opening log.txt...\n");
 	if ( (fp = fopen(LOG_NAME, "a") ) == NULL )
     {
         printf("Error opening log file: %d\n", fd);
         exit(1);
     }
-
-	struct sigaction sa;
-	sa.sa_handler = recibiSignal;
-	//sa.sa_flags = 0; //SA_RESTART;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1,&sa,NULL);
-    sigaction(SIGUSR2,&sa,NULL);
- 
-    pid=getpid();      //Process ID of itself
-  // kill(pid,SIGUSR1);        // Send SIGUSR1 to itself
-  printf("el pid es:  %d  \n", pid);
-
-
 
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1  )
@@ -71,9 +75,7 @@ int main(void)
         printf("Error creating named fifo: %d\n", returnCode);
         exit(1);
     }
-    
-
-
+   
     /* Open named fifo. Blocks until other process opens it */
 	printf("waiting for writers...\n");
 	if ( (fd = open(FIFO_NAME, O_RDONLY) ) < 0 )
@@ -88,6 +90,8 @@ int main(void)
     /* Loop until read syscall returns a value <= 0 */
 	do
 	{
+
+        
         /* read data into local buffer */
 		if ((bytesRead = read(fd, inputBuffer, BUFFER_SIZE)) == -1)
         {
@@ -96,7 +100,7 @@ int main(void)
         else
 		{
 			inputBuffer[bytesRead] = '\0';
-			printf("reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer);
+			printf("saving in log %s \n", inputBuffer);
             if (bytesRead != 0)
                 fprintf(fp,"DATA:%s \n", inputBuffer);
                 
