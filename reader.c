8c310f65f187aa1706919468341c480b00d1112d
Sergio Alberino <alberino@gmail.com>
2020-11-11 18:22:46 -0300

    segundo comit

--- reader.c
+++ reader.c
@@ -7,15 +7,64 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <signal.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define FIFO_NAME "myfifo"
+#define LOG_NAME "log.txt"
+#define SIGFILE_NAME "signals.txt"
 #define BUFFER_SIZE 300
+
+
+void recibiSignal(int sig)
+{
+  FILE *Sp;
+  /* Open signals.txt */
+	printf("abriendo archivo signals.txt...\n");
+	if ( (Sp = fopen(SIGFILE_NAME, "a") ) == NULL )
+    {
+        printf("Error opening signal file \n");
+        exit(1);
+    }
+  
+  if (sig==SIGUSR1)
+  printf("SIGN:1 \n");
+  fprintf(Sp,"SIGN:1 \n");
+  if (sig==SIGUSR2)
+  printf("SIGN:2 \n");
+  fprintf(Sp,"SIGN:2 \n");
+
+}
 
 int main(void)
 {
 	uint8_t inputBuffer[BUFFER_SIZE];
 	int32_t bytesRead, returnCode, fd;
-    
+    FILE *fp;
+    pid_t pid;
+
+ /* Open log.txt */
+	printf("abriendo archivo log.txt...\n");
+	if ( (fp = fopen(LOG_NAME, "a") ) == NULL )
+    {
+        printf("Error opening log file: %d\n", fd);
+        exit(1);
+    }
+
+	struct sigaction sa;
+	sa.sa_handler = recibiSignal;
+	//sa.sa_flags = 0; //SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1,&sa,NULL);
+    sigaction(SIGUSR2,&sa,NULL);
+ 
+    pid=getpid();      //Process ID of itself
+  // kill(pid,SIGUSR1);        // Send SIGUSR1 to itself
+  printf("el pid es:  %d  \n", pid);
+
+
+
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1  )
     {
@@ -23,6 +72,8 @@ int main(void)
         exit(1);
     }
     
+
+
     /* Open named fifo. Blocks until other process opens it */
 	printf("waiting for writers...\n");
 	if ( (fd = open(FIFO_NAME, O_RDONLY) ) < 0 )
@@ -46,9 +97,13 @@ int main(void)
 		{
 			inputBuffer[bytesRead] = '\0';
 			printf("reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer);
+            if (bytesRead != 0)
+                fprintf(fp,"DATA:%s \n", inputBuffer);
+                
 		}
 	}
 	while (bytesRead > 0);
 
 	return 0;
 }
+
